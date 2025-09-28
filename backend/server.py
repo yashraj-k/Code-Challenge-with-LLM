ad00b3750bc17bf3970f9da0c63cb74aa8a62609
yashraj-k <kabreyashraj221521@gmail.com>
2025-09-28 15:28:09 +0530

    feat: Add unprotected health check endpoint

--- backend/server.py
+++ backend/server.py
@@ -2,3 +2,7 @@ from src.app import  app
 if(__name__ == "__main__"):
     import uvicorn
     uvicorn.run(app,host="0.0.0.0", port=8000)
+
+@app.get("/api/health")
+def health_check():
+    return {"status": "ok", "message": "API is running"}
